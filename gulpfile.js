0914ea56c8168b1768f79cbd890fb56ce7d1c337
Volodymyr Gorbunov <deftime@gmail.com>
2023-06-05 20:56:46 +0300

    Run, install and first settings.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const {src, dest, parallel, series, watch} = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+
+function scripts() {
+    return src('./src/js/app.js')
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(dest('./public/js'))
+}
+
+function styles() {
+    return src('./src/scss/app.scss')
+        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(concat('styles.min.js'))
+        .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true }))
+        .pipe(dest('./public/css'))
+}
+
+function watcher() {
+    watch('./src/scss/**/*.scss', styles);
+    watch('./src/js/**/*.js', scripts);
+}
+
+exports.runStyles = styles;
+exports.runScripts = scripts;
+exports.watch = watcher;
+
+exports.default = parallel(styles, scripts);
