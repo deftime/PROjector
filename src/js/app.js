2ce99f0ffb08ba10e14f5feb41a0f7a70fc050b6
Volodymyr Gorbunov <deftime@gmail.com>
2023-06-08 22:11:43 +0300

    Desktop ready!

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ window.onload = function() {
 
     initSelectric();
     checkForm();
+    addRemoveFav();
+    checkFavorites();
 
 }
 
@@ -23,5 +25,33 @@ function checkForm() {
             this.value.length > 10 ? errMsg.style.display = 'block' : errMsg.style.display = 'none';
             this.value.length > 10 ? btn.setAttribute('disabled', 'true') : btn.removeAttribute('disabled');
         })
+    }
+}
+
+function addRemoveFav() {
+    let items = document.querySelectorAll('.list .item');
+
+    for (let key of items) {
+        let btn = key.querySelector('.js-favorites');
+
+        btn.addEventListener('click', function(){
+            if (localStorage.getItem(key.id)) {
+                localStorage.removeItem(key.id);
+                key.classList.remove('favorite');
+                this.querySelector('span').innerText = 'Add';
+            } else {
+                localStorage.setItem(key.id, true);
+                key.classList.add('favorite');
+                this.querySelector('span').innerText = 'Remove';
+            }
+        })
+    }
+}
+
+function checkFavorites() {
+    let items = document.querySelectorAll('.list .item');
+
+    for (let key of items) {
+        localStorage.getItem(key.id) ? key.classList.add('favorite') : undefined ;
     }
 }
